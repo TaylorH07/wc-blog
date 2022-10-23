c06531bda7a6aa544c7cffc4407a1ac52df916a8
taylor.holmes <taylorh2343@gmail.com>
2022-10-23 14:49:03 -0500

    completed nav bar

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -3,9 +3,26 @@ import './navbar.css'
 export default function navbar() {
   return (
       <div className='nav'>
-          <div className="topleft">l</div>
-          <div className="topCenter">c</div>
-          <div className="topRight">r</div>
+          <div className="navLeft">
+              <i className="navIcon fa-brands fa-square-facebook"></i>
+              <i className="navIcon fa-brands fa-square-twitter"></i>
+              <i className="navIcon fa-brands fa-square-pinterest"></i>
+              <i className="navIcon fa-brands fa-square-instagram"></i>
+          </div>
+          <div className="navCenter">
+              <ul className="navList">
+                  <li className='navListItem'>HOME</li>
+                  <li className='navListItem'>ABOUT</li>
+                  <li className='navListItem'>CONTACT</li>
+                  <li className='navListItem'>WRITE</li>
+                  <li className='navListItem'>LOGOUT</li>
+              </ul>
+          </div>
+          <div className="navRight">
+              <img className='navImg'
+                  src="https://images.pexels.com/photos/5912965/pexels-photo-5912965.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              <i className="navSearchIcon fa-solid fa-magnifying-glass"></i>
+          </div>
 
     </div>
   )
