7b4994ce455ff12d9c11e8e6938ef21bffcda7d1
taylor.holmes <taylorh2343@gmail.com>
2022-10-31 11:49:46 -0500

    completed header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,11 +1,16 @@
 import './header.css'
 
 export default function Header() {
-  return (
+    return (
         <div className='header'>
-          <div className="headerTitles">
-              
+            <div className="headerTitles">
+                <span className='headerTitleSm'> React and Node</span>
+                <span className='headerTitleLg'> Blog</span>
             </div>
+            <img className='headerImg'
+                src='https://images.pexels.com/photos/10242311/pexels-photo-10242311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                alt=''
+            />
         </div>
     )
 }
