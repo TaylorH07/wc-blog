09acd87fe818569f6100017d923f1d2064e39d0f
tayHolmes7 <taylorh2343@gmail.com>
2022-11-02 12:27:19 -0500

    completed sidebar

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -5,13 +5,34 @@ export default function Sidebar() {
         <div className='sidebar'>
             <div className="sidebarItem">
                 <span className="sidebarTitle"> ABOUT ME </span>
-                <img src="https://images.pexels.com/photos/13896385/pexels-photo-13896385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                <img className='sidebarPic' src="https://images.pexels.com/photos/13896385/pexels-photo-13896385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                     alt="" 
                 />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                     sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle"> CATEGORIES </span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle"> FOLLOW ME </span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+                    <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+                    <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+                    <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+                </div>
             </div>
         </div>
     )
