f7a4fab966acb7359ac7ce8339311400d433d3b6
tayHolmes7 <taylorh2343@gmail.com>
2022-11-13 14:13:16 -0600

    connecting pages and routes on app.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ import WritePost from './pages/writePost/WritePost';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
 import Register from './register/Register';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <NavBar/>
-      <Register/>
-    </>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/register">
+          <Register/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>        
+        <Route path="/writePost">
+          <WritePost/>
+        </Route>        
+        <Route path="/settings">
+          <Settings/>
+        </Route>
+        <Route path="/post/:postId">
+          <SinglePage/>
+        </Route>   
+      </Switch>
+    </Router>
   );
 }
 
