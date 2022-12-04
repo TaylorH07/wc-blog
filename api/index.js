7914f0cd8567645d68980a4c7b0cc89827360070
tayHolmes7 <taylorh2343@gmail.com>
2022-12-04 13:43:56 -0600

    began adding auth routes

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+const router = require('express').Router();
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+    .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIntex: true
@@ -13,6 +17,10 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log());
 
+app.use("/api/auth", auth);
+
 app.listen("2121", () => {
     console.log("Backend is running.");
 });
+
+module.exports = router 
